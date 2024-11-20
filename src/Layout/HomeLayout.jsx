09bcc87450a2ea942d0eae89f1cd1916ccecd8b5
reham0000 <amsarkerreham@gmail.com>
2024-11-20 21:16:20 +0600

    first commit

---
+++ src/Layout/HomeLayout.jsx
@@ -0,0 +1,26 @@
+import Header from "../Components/Header";
+import LatestNews from "../Components/LatestNews";
+import Navbar from "../Components/Navbar";
+
+const HomeLayout = () => {
+  return (
+    <div className="font-poppins">
+      <header>
+        <Header></Header>
+        <section className="w-11/12 mx-auto">
+          <LatestNews></LatestNews>
+        </section>
+      </header>
+      <nav className="w-11/12 mx-auto py-2">
+        <Navbar></Navbar>
+      </nav>
+      <main className="w-11/12 mx-auto pt-5 gap-3 md:grid grid-cols-12">
+        <aside className="left col-span-3">Left Navbar</aside>
+        <section className="col-span-6">Main Content</section>
+        <aside className="col-span-3">Right Navbar</aside>
+      </main>
+    </div>
+  );
+};
+
+export default HomeLayout;
